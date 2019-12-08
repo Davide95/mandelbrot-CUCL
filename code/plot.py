ca09a2ec940a0d8b1dd7a6b232c5e468b91db6bb
Eros <you@example.com>
2019-12-08 12:52:49 +0100

    Colored version

--- code/plot.py
+++ code/plot.py
@@ -2,12 +2,21 @@ import sys
 import logging
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+import math
 
 if len(sys.argv) < 2:
     logging.critical('Please specify the output file as a parameter.')
     exit(-1)
 
 data = pd.read_csv(sys.argv[1], header=None, index_col=False).to_numpy()
-plt.imshow(data)
+
+rgb_image = np.zeros((2000, 3000, 3), dtype=np.uint8)
+
+rgb_image[:, :, 0] = data*data * 255
+rgb_image[:, :, 1] = data*data * 255
+rgb_image[:, :, 2] = data * 255
+
+plt.imshow(rgb_image)
 plt.axis('off')
 plt.show()
--- code/sequential.cpp
+++ code/sequential.cpp
@@ -20,17 +20,19 @@
 
 #define STEP ((double)RATIO_X / WIDTH)
 
-#define DEGREE 2 // Degree of the polynomial
+#define DEGREE 2        // Degree of the polynomial
 #define ITERATIONS 1000 // Maximum number of iterations
 
 using namespace std;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     int *const image = new int[HEIGHT * WIDTH];
 
     const auto start = chrono::steady_clock::now();
-    for(int pos = 0; pos < HEIGHT * WIDTH; pos++) {
-        image[pos] = 1;
+    for (int pos = 0; pos < HEIGHT * WIDTH; pos++)
+    {
+        image[pos] = 0;
 
         const int row = pos / WIDTH;
         const int col = pos % WIDTH;
@@ -38,49 +40,53 @@ int main(int argc, char **argv) {
 
         // z = z^2 + c
         complex<double> z(0, 0);
-        for(int i = 0; i < ITERATIONS; i++) {
+        for (int i = 0; i < ITERATIONS; i++)
+        {
             z = pow(z, 2) + c;
 
             // If it is convergent
-            if(abs(z) >= 2) {
-                image[pos] = 0;
+            if (abs(z) >= 2)
+            {
+                image[pos] = i;
                 break;
             }
         }
     }
     const auto end = chrono::steady_clock::now();
-    cout << "Time elapsed: " 
+    cout << "Time elapsed: "
          << chrono::duration_cast<chrono::seconds>(end - start).count()
          << " seconds." << endl;
-
 
     // Write the result to a file
     ofstream matrix_out;
 
-    if(argc < 2) {
+    if (argc < 2)
+    {
         cout << "Please specify the output file as a parameter." << endl;
         return -1;
     }
 
     matrix_out.open(argv[1], ios::trunc);
-    if(!matrix_out.is_open()) {
+    if (!matrix_out.is_open())
+    {
         cout << "Unable to open file." << endl;
         return -2;
     }
 
-    for(int row = 0; row < HEIGHT; row++) {
-        for(int col = 0; col < WIDTH; col++) {
+    for (int row = 0; row < HEIGHT; row++)
+    {
+        for (int col = 0; col < WIDTH; col++)
+        {
             matrix_out << image[row * WIDTH + col];
 
-            if(col < WIDTH -1)
+            if (col < WIDTH - 1)
                 matrix_out << ',';
         }
-        if(row < HEIGHT - 1)
+        if (row < HEIGHT - 1)
             matrix_out << endl;
     }
     matrix_out.close();
 
-
-    delete []image; // It's here for coding style, but useless
+    delete[] image; // It's here for coding style, but useless
     return 0;
 }
